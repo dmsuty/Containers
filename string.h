0e6547385cc73f9d04c4e7fa79508c7c2578180b
dmsuty <dmsutyy@gmail.com>
2021-11-16 00:56:48 +0300

    string and sum_of_products

---
+++ string.h
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <cstring>
+
+class String;
+
+String operator+ (const String& s1, const String& s2);
+
+String operator+ (const String& s, const char& c);
+
+String operator+ (const char& c, const String& s);
+
+bool operator== (const String& s1, const String& s2);
+
+class String {
+
+private:
+    char* str;
+    size_t sz;
+    size_t capacity;
+
+    void doubling() {
+        capacity *= 2;
+        char* new_str = new char[capacity];
+        memcpy(new_str, str, sz);
+        delete[] str;
+        str = new_str;
+    }
+
+    void halve() {
+        capacity /= 2;
+        char* new_str = new char[capacity];
+        memcpy(new_str, str, sz);
+        delete[] str;
+        str = new_str;
+    }
+
+    void swap(String &s) {
+        std::swap(sz, s.sz);
+        std::swap(str, s.str);
+        std::swap(capacity, s.capacity);
+    }
+ 
+public:
+    String(): str(new char[1]), sz(0), capacity(1) {}
+
+    String(const char* s) {
+        size_t s_size = 0;
+        while (s[s_size] != '\0') {
+            ++s_size;
+        }
+        sz = s_size;
+        capacity = s_size * 2;
+        str = new char[capacity];
+        memcpy(str, s, sz);
+    }
+
+    String(const size_t& new_sz, const char& c):
+        str(new char[new_sz * 2]), sz(new_sz), capacity(new_sz * 2) {
+        memset(str, c, new_sz);
+    }
+
+    String(const String& s): String(s.sz, '\0') {
+        memcpy(str, s.str, sz);
+    }
+
+    ~String() {
+        delete[] str;
+    }
+
+    String& operator=(const String& s) {
+        String copy = s;
+        swap(copy);
+        return *this;
+    }
+
+    void push_back(const char& c) {
+        if (capacity == sz) {
+            doubling();
+        }
+        str[sz] = c;
+        ++sz;
+    }
+
+    void pop_back() {
+        if (sz * 4 <= capacity) {
+            halve();
+        }
+        --sz;
+    }
+
+    String& operator+= (const char& c) {
+        push_back(c);
+        return *this;
+    }
+
+    String& operator+= (const String &s) {
+        size_t start_size = s.sz;
+        for (size_t i = 0; i < start_size; ++i) {
+            *this += s.str[i];
+        }
+        return *this;
+    }
+
+    char& operator[] (const size_t& i) {
+        return str[i];
+    }
+
+    const char& operator[] (const size_t& i) const {
+        return str[i];
+    }
+
+    char& front() {
+        return str[0];
+    }
+
+    const char& front() const {
+        return str[0];
+    }
+
+    char& back() {
+        return str[sz - 1];
+    }
+
+    const char& back() const {
+        return str[sz - 1];
+    }
+
+    size_t length() const {
+        return sz;
+    }
+
+    bool empty() const {
+        return sz == 0;
+    }
+
+    void clear() {
+        *this = String();
+    }
+
+    String substr(const size_t& start, const size_t& count) const {
+        String substring(count, '0');
+        memcpy(substring.str, str + start, count);
+        return substring;
+    }
+
+    size_t find(const String& substring) const {
+        for (int i = 0; i + substring.sz <= sz; ++i) {
+            if (substr(i, substring.sz) == substring) {
+                return i;
+            }
+        }
+        return sz;
+    }
+
+    size_t rfind(const String& substring) const {
+        int ret = sz;
+        for (int i = 0; i + substring.sz <= sz; ++i) {
+            if (substr(i, substring.sz) == substring) {
+                ret = i;
+            }
+        }
+        return ret;
+    }
+
+    friend std::istream& operator>> (std::istream& in, const String& point);
+};
+
+
+String operator+(const String& s1, const String& s2) {
+    String sum(s1);
+    sum += s2;
+    return sum;
+}
+
+
+String operator+(const String& s, const char& c) {
+    return s + String(1, c);
+}
+
+
+String operator+(const char& c, const String& s) {
+    return String(1, c) + s;
+}
+
+
+bool operator==(const String& s1, const String& s2) {
+    if (s1.length() != s2.length()) {
+        return false;
+    }
+    bool equil = true;
+    for (size_t i = 0; i < s1.length(); ++i) {
+        if (s1[i] != s2[i]) {
+            equil = false;
+            break;
+        }
+    }
+    return equil;
+}
+
+
+std::ostream& operator<< (std::ostream& out, const String& s) {
+    for (size_t i = 0; i < s.length(); ++i) {
+        out << s[i];
+    }
+    return out;
+}
+
+
+std::istream& operator>> (std::istream& in, String& s) {
+    s.clear();
+    char c = in.get();
+    while (isspace(c)) {
+        c = in.get();
+    }
+    while (!isspace(c) && c != EOF  ) {
+        s.push_back(c);
+        c = in.get();
+    }
+    return in;
+}
---
+++ sum_of_products.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+
+int get_num(char* s) {
+    int ret = 0;
+    for (int i = 0; s[i] != '\0'; ++i) {
+        ret = ret * 10 + static_cast<int>(s[i]) - static_cast<int>('0');
+    }
+    return ret;
+}
+
+
+bool check(int argc, int* positions) {
+    bool correct = true;
+    for (int i = 0; i < argc; ++i) {
+        for (int j = i + 1; j < argc; ++j) {
+            if (positions[i] == positions[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+long long sum_of_products(int argc, int* positions, int** arrays) {
+    long long ret = 1;
+    for (int i = 0; i < argc; ++i) {
+        ret *= arrays[i][positions[i]];
+    }
+    return ret;
+}
+
+
+long long solve(int argc, int* sizes, int** arrays, int cur_pos, int* positions=nullptr) {
+    if (cur_pos == argc && check(argc, positions)) {
+        long long ret = sum_of_products(argc, positions, arrays);
+        delete[] positions;
+        return ret;
+    }
+    long long ret = 0;
+    int** new_slay = new int*[sizes[cur_pos]];
+    for (int i = 0; i < sizes[cur_pos]; ++i) {
+        new_slay[i] = new int[cur_pos + 1];
+        for (int j = 0; j < cur_pos; ++j) {
+            new_slay[i][j] = positions[j];
+        }
+        new_slay[i][cur_pos] = i;
+    }
+    delete[] positions;
+    for (int i = 0; i < sizes[cur_pos]; ++i) {
+        ret += solve(argc, sizes, arrays, cur_pos + 1, new_slay[i]);
+    }
+    return ret;
+}
+
+
+int main(int argc, char** argv) {
+    --argc;
+    int* sizes = new int[argc];
+    int** arrays = new int*[argc];
+    for (int i = 0; i < argc; ++i) {
+        int cur_array_size = get_num(argv[i + 1]);
+        sizes[i] = cur_array_size;
+        arrays[i] = new int[cur_array_size];
+        for (int j = 0; j < cur_array_size; ++j) {
+            std::cin >> arrays[i][j];
+        }
+    }
+    std::cout << solve(argc, sizes, arrays, 0) << '\n';
+}
