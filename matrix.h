f21b9a910ba733ca4ae681f574713937f4015139
dmsuty <dmsutyy@gmail.com>
2022-01-28 01:58:46 +0300

    added some code, nothing special

--- matrix.h
+++ matrix.h
@@ -7,37 +7,64 @@
 #include "biginteger.h"
 
 
+template<int N, int K>
+class Is_prime_helper {
+    //static const bool value = N % K == 0 && 
+};
+
+
 template<size_t N>
 class Residue {
 private:
-   size_t value=0;
-
-public:
-   Residue& operator+= (const Residue& other) {
-       value += other.value;
-       value %= N;
-   }
+    size_t value=0;
  
-   Residue& operator-= (const Residue& other) {
-       value += N - other.value;
-       value %= N;
-   } 
+public:
+    Residue() = default; //почему нельзя без этого
 
-   Residue& operator*= (const Residue& other) {
-       value *= other.value;
-       value %= N;
-   }    
+    explicit Residue(int number): value((N + number % N) % N) {} 
 
-   Residue& operator/= (const Residue& other) {
+    Residue& operator+= (const Residue& other) {
+        value += other.value;
+        value %= N;
+        return *this;
+    }
+  
+    Residue& operator-= (const Residue& other) {
+        value += N - other.value;
+        value %= N;
+        return *this;
+    } 
+ 
+    Residue& operator*= (const Residue& other) {
+        value *= other.value;
+        value %= N;
+        return *this;
+    }    
+ 
+    Residue power(size_t power_value) const {
+        if (!power_value) {
+            return *this;
+        }
+        if (power_value % 2 == 0) {
+            return power(power_value / 2) * power(power_value / 2); 
+        }
+        return *this * power(power_value - 1);
+    }
+ 
+    Residue& operator/= (const Residue& other) {
+        *this *= other.power(N - 2);
+        return *this;
+    }
 
-   }
+    explicit operator int() {
+        return value;
+    }
 };
 
 
 template<size_t Height, size_t Width, typename Field=Rational>
 class Matrix {
 private:
-    Field matrix[Height][Width];
 
 public:
     
