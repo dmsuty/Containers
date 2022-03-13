c3ed5c3896e1749dd6687c5b9e7ebd3214a8eff3
dmsuty <dmsutyy@gmail.com>
2022-03-13 14:16:25 +0300

    made iterators, but it is not compiling(

--- deque.h
+++ deque.h
@@ -1,21 +1,25 @@
 #include <iostream>
-#include <vector>
 
 template<typename T>
 class Deque {
 
 private:
-
   template<bool is_const>
   struct basic_iterator {
   private:
-    T* array_pointer_;
+    using ValueType = std::conditional<is_const, const T, T>;
+    ValueType* array_pointer_;
     size_t index_;
+
+    basic_iterator(ValueType* array_pointer, size_t index):
+      array_pointer_(array_pointer), index_(index) {}
+
   public:
     basic_iterator& operator++ () {
       ++index_;
       if (index_ == kArray_size_) {
         index_ = 0;
+        ++array_pointer_;
       }
       return *this;
     }
@@ -23,80 +27,141 @@ private:
     basic_iterator operator++ (int) {
       basic_iterator result = *this;
       ++(*this);
-      adsjfalskj lfj;laksj;
       return result;
     }
+
+    basic_iterator& operator-- () {
+      if (index_ == 0) {
+        index_ = kArray_size_ - 1;
+        --array_pointer_;
+      } else {
+        --index_;
+      }
+      return *this;
+    }
+
+    basic_iterator operator-- (int) {
+      basic_iterator result = *this;
+      --(*this);
+      return result;
+    }
+
+    int operator- (const basic_iterator& other) {
+      int backets_between = array_pointer_ - other.array_pointer_;
+      return backets_between * kArray_size_ + index_ - other.index_;
+    }
+
+    bool operator< (const basic_iterator& other) const {
+      return *this - other < 0;
+    }
+
+    bool operator== (const basic_iterator& other) const {
+      return *this - other == 0;
+    }
+
+    bool operator> (const basic_iterator& other) const {
+      return other < *this;
+    }
+
+    bool operator!= (const basic_iterator& other) const {
+      return !(*this == other);
+    }
+
+    bool operator<= (const basic_iterator& other) const {
+      return !(*this > other);
+    }
+
+    bool operator>= (const basic_iterator& other) const {
+      return !(*this < other);
+    }
+
+    ValueType operator* () const {
+      return (*array_pointer_)[index_];
+    }
+
+    ValueType* operator-> () const {
+      return *array_pointer_;
+    }
   };
+
 
 public:
   using iterator = basic_iterator<false>;
   using const_iterator = basic_iterator<true>;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 private:
   static const size_t kArray_size_ = sizeof(T) * 1000;
   T** arrays_;
   iterator begin_;
   iterator end_;
-  size_t size_;
+  size_t backets_count_;
+
+  void expand() {
+    //TODO
+  }
+
+  void compress() {
+    //TODO
+  }
 
 public:
-
-  Deque() noexcept: arrays_(new T*[1]), size_(0) {
+  Deque(): arrays_(new T*[1]), begin_(iterator(arrays_[0], -1)), 
+    end_(iterator(arrays_[0]), 0), backets_count_(1) {
     arrays_[0] = new uint8_t[kArray_size_];
   }
 
-  Deque(int size): Deque() {
-    for (int i = 0; i < size; ++i) {
+  Deque(int new_size): Deque() {
+    for (int i = 0; i < new_size; ++i) {
       (*this).push_back(T());
     }
   }
 
-  Deque(int size, const T& element): {
-
+  Deque(int new_size, const T& element): Deque() {
+    for (int i = 0; i < new_size; ++i) {
+      (*this).push_back(element);
+    }
   } 
 
   size_t size() const {
-    return size_;
+    return end_ - begin_ - 1;
   }
 
   T& operator[] (size_t i) noexcept {
-    //some code
+    //TODO
   }
 
   const T& operator[] (size_t i) const noexcept {
-    //some code
+    //TODO
   }
 
   T& at(size_t i) {
-    //some code
+    //TODO
   }
 
   const T& at(size_t i) const  {
-    //some code
+    //TODO
   }
 
   void push_back(T element) {
-    //some code
-    ++size_;
+    //TODO
     ++end_;
   }
 
   void push_front(T element) {
-    ++size_;
     --begin_;
-    //some code
+    //TODO
   }
 
   void pop_back() {
-    //some code
+    //TODO
     --end_;
-    --size_;
   }
 
   void pop_front() {
-    //some code
+    //TODO
     ++begin_;
-    --size_;
   }
 
   iterator begin() {
@@ -107,9 +172,16 @@ public:
     return end_;
   }
 
+  iterator rbein() {
+  }
+
+  iterator rend() {
+  }
+
   const_iterator cbegin() const {
   }
 
   const_iterator cend() const {
   }
 };
+  
