5139b4e0c69354238c8b1f4bf0b5221aac5e1e6a
dmsuty <dmsutyy@gmail.com>
2022-03-15 18:34:40 +0300

    made some work

--- deque.h
+++ deque.h
@@ -6,15 +6,13 @@ class Deque {
 private:
   template<bool is_const>
   struct basic_iterator {
-  private:
-    using ValueType = std::conditional<is_const, const T, T>;
+    using ValueType = std::conditional_t<is_const, const T, T>;
     ValueType* array_pointer_;
     size_t index_;
 
     basic_iterator(ValueType* array_pointer, size_t index):
       array_pointer_(array_pointer), index_(index) {}
 
-  public:
     basic_iterator& operator++ () {
       ++index_;
       if (index_ == kArray_size_) {
@@ -51,6 +49,21 @@ private:
       return backets_between * kArray_size_ + index_ - other.index_;
     }
 
+    basic_iterator& operator+= (int step) {
+      step += index_;
+      index_ = 0;
+      int backets_step = step / kArray_size_;
+      array_pointer_ += backets_step;
+      index_ = step % kArray_size_;
+      return *(this);
+    }
+
+    basic_iterator operator+ (int step) {
+      basic_iterator result = (*this);
+      result += step;
+      return result;
+    }
+
     bool operator< (const basic_iterator& other) const {
       return *this - other < 0;
     }
@@ -80,7 +93,7 @@ private:
     }
 
     ValueType* operator-> () const {
-      return *array_pointer_;
+      return array_pointer_;
     }
   };
 
@@ -92,24 +105,41 @@ public:
   using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 private:
-  static const size_t kArray_size_ = sizeof(T) * 1000;
+  static const size_t kArray_size_ = 1000;
+  static const size_t kByte_Array_size_ = sizeof(T) * kArray_size_;
   T** arrays_;
   iterator begin_;
   iterator end_;
   size_t backets_count_;
 
-  void expand() {
+  void Expand() {
+    
+  }
+
+  bool ShouldCompress() {
+    return size() * 9 < backets_count_ * kArray_size_;
+  }
+
+  void Compress() {
     //TODO
   }
 
-  void compress() {
-    //TODO
+  iterator ToIter(size_t i) {
+    return begin_ + i;
+  }
+
+  bool FreePlaceBack() {
+    return (begin_.array_pointer == arrays_ && begin_.index_ == 0);
+  }
+
+  bool FreePlaceFront() {
+    return (end_.array_pointer == arrays_ + backets_count_);
   }
 
 public:
   Deque(): arrays_(new T*[1]), begin_(iterator(arrays_[0], -1)), 
-    end_(iterator(arrays_[0]), 0), backets_count_(1) {
-    arrays_[0] = new uint8_t[kArray_size_];
+    end_(iterator(arrays_[0], 0)), backets_count_(1) {
+    arrays_[0] = reinterpret_cast<T*>(new uint8_t[kByte_Array_size_]);
   }
 
   Deque(int new_size): Deque() {
@@ -125,43 +155,56 @@ public:
   } 
 
   size_t size() const {
-    return end_ - begin_ - 1;
+    return end_ - begin_;
+  }
+
+  bool empty() const {
+    return size() == 0;
   }
 
   T& operator[] (size_t i) noexcept {
-    //TODO
+    return *ToIter(i);
   }
 
   const T& operator[] (size_t i) const noexcept {
-    //TODO
+    return *ToIter(i);
   }
 
   T& at(size_t i) {
-    //TODO
+    return (*this)[i];
   }
 
   const T& at(size_t i) const  {
-    //TODO
+    return (*this)[i];
   }
 
-  void push_back(T element) {
-    //TODO
+  void push_back(const T& element) {
+    if (!FreePlaceBack()) {
+      Expand();
+    }
     ++end_;
   }
 
-  void push_front(T element) {
+  void push_front(const T& element) {
+    if (!FreePlaceFront()) {
+      Expand();
+    }
     --begin_;
-    //TODO
+    new(begin_._array_pointer[begin_.index_]) T(element);
   }
 
   void pop_back() {
-    //TODO
     --end_;
+    if (ShouldCompress()) {
+      Compress();
+    }
   }
 
   void pop_front() {
-    //TODO
     ++begin_;
+    if (ShouldCompress()) {
+      Compress();
+    }
   }
 
   iterator begin() {
@@ -172,16 +215,28 @@ public:
     return end_;
   }
 
-  iterator rbein() {
+  iterator rbegin() {
+    return std::reverse_iterator<iterator>(end_ - 1);
   }
 
   iterator rend() {
+    //TODO
   }
 
   const_iterator cbegin() const {
+    return const_iterator(begin_);
   }
 
   const_iterator cend() const {
+    return const_iterator(end_);
+  }
+
+  const_reverse_iterator crbegin() const {
+    //TODO
+  }
+
+  const_reverse_iterator crend() const {
+    //TODO
   }
 };
   
